caddacda683c9124068adf51ae86d26ccc542aae
black-flag <strongxj@126.com>
2021-08-04 15:42:15 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,1 +1,8 @@
 from django.http import HttpResponse
+from djangoshortcuts import redirect
+
+def index(request):
+    return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
